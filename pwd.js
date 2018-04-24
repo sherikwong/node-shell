3356cdb4428b441cc79cfea040fa0b0a5de2b120
marisaoppenheim <marisa.oppenheim@yahoo.com>
2018-04-24 12:42:14 -0400

    pwd module

---
+++ pwd.js
@@ -0,0 +1,10 @@
+module.export = function () {
+    if (cmd == "pwd") {
+    const cmd = `${process.cwd()}`;
+    process.stdout.write(cmd); // Outputs what we typed
+    process.stdout.write('\nprompt > '); // Asks for new prompt
+  } else {
+    process.stdout.write('You typed: ' + cmd); // Outputs what we typed
+    process.stdout.write('\nprompt > '); // Asks for new prompt
+  }
+}
